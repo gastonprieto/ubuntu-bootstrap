1b6a088b35fb42b317e0193f3edbd9355429b5e2
Gaston Prieto <gaston@producteca.com>
2023-02-09 00:26:07 -0300

    Using .zshrc from repository

--- default/zsh.sh
+++ default/zsh.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+GITHUB_ZSH_URL=https://raw.githubusercontent.com/gastonprieto/ubuntu-bootstrap/main/resources/.zshrc
+
 # Zsh
 sudo apt-get install -y zsh autojump && \
 chsh -s /usr/bin/zsh && \
 mkdir -p $APPS/antigen && \
 curl -L git.io/antigen > $APPS/antigen/antigen.zsh && \
-wget -q https://gist.githubusercontent.com/gastonprieto/9fdf398cf680b1931f994ae853aa3867/raw/1e83ef6a73b3d3d43e3cc74db77f1f4ea51d775d -O $ZSH_FILE && \
+wget -q $GITHUB_ZSH_URL -O $ZSH_FILE && \
 echo "" >> $ZSH_FILE
