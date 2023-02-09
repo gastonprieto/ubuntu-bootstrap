1ba1945baf021627b70f428867e2cfdcead43eb6
Gaston Prieto <gaston@producteca.com>
2023-02-09 00:32:26 -0300

    Adding git-aliases

---
+++ dev/git-aliases.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# common aliases
+git config --global alias.st "status -sb"
+git config --global alias.br "branch"
+git config --global alias.ci "commit"
+git config --global alias.co "checkout"
+git config --global alias.di "diff"
+git config --global alias.uncommit "reset HEAD~1"
+git config --global alias.last-change "diff HEAD^"
+
+# not so common aliases
+git config --global alias.ch    "diff --cached"
+git config --global alias.put   "push origin HEAD"
+git config --global alias.track "checkout --track"
+git config --global alias.fe    "fetch origin"
+git config --global alias.ap    "apply"
+
+# miscellaneous aliases
+git config --global alias.opts "config --list"
+git config --global alias.logg "log --pretty=oneline --graph --abbrev-commit"
+git config --global alias.log2 "log --pretty=format:'%an :: %s' --graph"
+
+# stashing aliases
+git config --global alias.spit "stash show -p"
+git config --global alias.list "stash list"
+git config --global alias.save "stash save"
+git config --global alias.drop "stash drop"
+git config --global alias.pop  "stash pop"
+git config --global alias.sap  "stash apply"
+git config --global alias.stash-unapply '!git stash show -p | git apply -R'
+git config --global alias.overwrite-with-remote '!f(){ ACTUAL_BRANCH=`git rev-parse --abbrev-ref HEAD`; git fe; git reset --hard origin/$ACTUAL_BRANCH;}; f'
+
+# hardcore-stashing aliases
+git config --global alias.spit0 "stash show -p stash@{0}"
+git config --global alias.spit1 "stash show -p stash@{1}"
+git config --global alias.spit2 "stash show -p stash@{2}"
+git config --global alias.spit3 "stash show -p stash@{3}"
+git config --global alias.spit4 "stash show -p stash@{4}"
+git config --global alias.spit5 "stash show -p stash@{5}"
+git config --global alias.spit6 "stash show -p stash@{6}"
+
+git config --global alias.drop0 "stash drop stash@{0}"
+git config --global alias.drop1 "stash drop stash@{1}"
+git config --global alias.drop2 "stash drop stash@{2}"
+git config --global alias.drop3 "stash drop stash@{3}"
+git config --global alias.drop4 "stash drop stash@{4}"
+git config --global alias.drop5 "stash drop stash@{5}"
+git config --global alias.drop6 "stash drop stash@{6}"
--- dev/git.sh
+++ dev/git.sh
@@ -1,11 +1,15 @@
+#!/bin/bash
+
 # GIT
 sudo apt-get install -y git && \
-
-wget -qO- https://gist.githubusercontent.com/gastonprieto/2369819/raw/2bae8194a26663da7383e09ff3b38cb18fc6586c/git_aliases.txt | sh && \
+./git-aliases.sh && \
 git config --global core.editor vim && \
-git config --global diff.tool meld && \
+git config --global diff.tool meld
+
+# Github CLI
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg && \
 sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg && \
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null && \
 sudo apt update && sudo apt install gh && \
 gh auth login
+
