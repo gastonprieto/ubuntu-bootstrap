d0c41dbd4b42f4b618d708c8594f283d09ea87d8
Gaston Prieto <gaston@producteca.com>
2023-02-09 12:02:29 -0300

    Adding starship

--- default/zsh.sh
+++ default/zsh.sh
@@ -9,3 +9,14 @@ mkdir -p $APPS/antigen && \
 curl -L git.io/antigen > $APPS/antigen/antigen.zsh && \
 wget -q $GITHUB_ZSH_URL -O $ZSH_FILE && \
 echo "" >> $ZSH_FILE
+
+# fzf
+sudo apt install fzf
+
+# starship
+curl -sS https://starship.rs/install.sh | sh
+
+echo "# Starship" >> $ZSH_FILE
+echo "" >> $ZSH_FILE
+echo "eval "$(starship init zsh)"" >> $ZSH_FILE
+echo "" >> $ZSH_FILE
