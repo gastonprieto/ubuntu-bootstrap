bfe4edbcff36baa5d7c404330090689375a07986
Gaston Prieto <gaston@producteca.com>
2023-02-27 12:39:51 -0300

    Adding dbeaver

--- databases/clients.sh
+++ databases/clients.sh
@@ -5,3 +5,9 @@ snap install robomongo
 
 # Redis-Client
 snap install redis-desktop-manager
+
+# Dbeaver
+curl -fsSL https://dbeaver.io/debs/dbeaver.gpg.key | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/dbeaver.gpg
+echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+
+sudo apt update && sudo apt install dbeaver-ce
